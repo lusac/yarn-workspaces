f12a88eec76bb2c6ed6fb85aba89e245edf61a59
Lucas Chuairi <lucas.chuairi@typeform.com>
2021-11-15 17:40:59 +0100

    delete

--- carta-api/carta-api/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '445efc1bfb5bbab5823c68faa5ff6d62'),
-    },
-  },
-});
